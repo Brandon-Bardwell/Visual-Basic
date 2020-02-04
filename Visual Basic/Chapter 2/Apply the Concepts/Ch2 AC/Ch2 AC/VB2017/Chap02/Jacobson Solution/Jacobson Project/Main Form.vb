784d5cee2fe798f43563db01a5618733ce164f1f
brandon bardwell <brandonbpcyt@gmail.com>
2020-02-04 10:30:01 -0600

    added homework

---
+++ Visual Basic/Chapter 2/Apply the Concepts/Ch2 AC/Ch2 AC/VB2017/Chap02/Jacobson Solution/Jacobson Project/Main Form.vb
@@ -0,0 +1,34 @@
+﻿' Name:         Jacobson Solution
+' Purpose:      Calculate and display the sales tax and total due.
+' Programmer:   Brandon Bardwell on 2-4-2020
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub lblTotal_Click(sender As Object, e As EventArgs) Handles lblTotal.Click
+
+    End Sub
+
+    Private Sub lblTotal_Enter(sender As Object, e As EventArgs) Handles lblTotal.Enter
+
+
+    End Sub
+
+    Private Sub txtSales_TextChanged(sender As Object, e As EventArgs) Handles txtSales.TextChanged
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+    End Sub
+End Class
